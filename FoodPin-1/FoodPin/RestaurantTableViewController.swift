6c46a037769749b66ff2b4d425f7e201045e639b
Ashigirl_96 <reonreon3reon@gmail.com>
2015-01-03 00:27:59 +0900

    でばっぐおなしゃす

---
+++ FoodPin-1/FoodPin/RestaurantTableViewController.swift
@@ -0,0 +1,208 @@
+//
+//  RestaurantTableViewController.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 11/8/14.
+//  Copyright (c) 2014 AppCoda. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+import Social
+import Twitter
+
+class RestaurantTableViewController: UITableViewController {
+    
+    @IBAction func Share2Facebook(){
+        var Share2Facebook : SLComposeServiceViewController = SLComposeServiceViewController(forServiceType:SLServiceTypeFacebook)
+        self.presentViewController(Share2Facebook, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func Share2Twitter(){
+        var Share2Twitter : SLComposeServiceViewController = SLComposeServiceViewController(forServiceType:SLServiceTypeTwitter)
+        self.presentViewController(Share2Twitter, animated: true, completion: nil)
+        
+    }
+    */
+    
+    
+    func tweet(message: String? = nil, image: UIImage? = nil, url: NSURL? = nil) {
+        
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            let controller = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            controller.setInitialText(message)
+            controller.addImage(image)
+            controller.addURL(url)
+            controller.completionHandler = { (result:SLComposeViewControllerResult) -> Void in
+                switch result {
+                case SLComposeViewControllerResult.Cancelled:
+                    NSLog("result: cancelled")
+                case SLComposeViewControllerResult.Done:
+                    // TODO: ADD SOME CODE FOR SUCCESS
+                    NSLog("result: done")
+                }
+            }
+            
+            self.presentViewController(controller, animated: true, completion: { () -> Void in
+                // controller is presented... do something if needed
+            })
+        } else {
+            NSLog("Twitter is not available")
+        }
+        
+    }
+    
+    
+    var restaurantNames = ["すっぴんメイク","ひげ男メイク","メガネメイク"]
+    
+    var restaurantImages = ["manome.png","reon.png","nakahara.png"]
+
+    var restaurantLocations = ["manome","reon","nakahara"]
+    
+    var restaurantTypes = ["2015/2/10", "2014/11/3", "2013/3/4"]
+
+    var restaurantIsVisited = [Bool](count: 21, repeatedValue: false)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return self.restaurantNames.count
+    }
+
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as CustomTableViewCell
+        
+        // Configure the cell...
+        cell.nameLabel.text = restaurantNames[indexPath.row]
+        cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
+        cell.locationLabel.text = restaurantLocations[indexPath.row]
+        cell.typeLabel.text = restaurantTypes[indexPath.row]
+//        cell.accessoryType = restaurantIsVisited[indexPath.row] ? .Checkmark : .None
+        cell.favorIconImageView.hidden = !restaurantIsVisited[indexPath.row]
+
+        // Circular image
+        cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.frame.size.width / 2
+        cell.thumbnailImageView.clipsToBounds = true
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        // Create an option menu as an action sheet
+        let optionMenu = UIAlertController(title: nil, message: "Menu", preferredStyle: .ActionSheet)
+        
+        // Add Cancel action to the menu
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        optionMenu.addAction(cancelAction)
+        
+        // Add Call action to the menu
+        let callActionHandler = { (action:UIAlertAction!) -> Void in
+            
+            let alertMessage = UIAlertController(title: "Service Unavailable", message: "Sorry, the call feature is not available yet. Please retry later.", preferredStyle: .Alert)
+            alertMessage.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alertMessage, animated: true, completion: nil)
+            
+        }
+        /*
+        let callAction = UIAlertAction(title: "Call " + "123-000-\(indexPath.row)", style: UIAlertActionStyle.Default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        */
+        
+        let callAction = UIAlertAction(title: "Put into FavList", style: UIAlertActionStyle.Default, handler: callActionHandler)
+        optionMenu.addAction(callAction)
+        
+        
+        
+        // Add I've been here action to the menu
+        let isVisitedTitle = "Listen" //restaurantIsVisited[indexPath.row] ? "I haven't been to here before" : "I've been here"
+        let isVisitedAction = UIAlertAction(title: isVisitedTitle, style: .Default, handler: {
+            (action:UIAlertAction!) -> Void in
+            
+            let cell = tableView.cellForRowAtIndexPath(indexPath) as CustomTableViewCell
+            
+            // Toggle checkmark
+            self.restaurantIsVisited[indexPath.row] = self.restaurantIsVisited[indexPath.row] ? false : true
+//            cell.accessoryType = self.restaurantIsVisited[indexPath.row] ? .Checkmark : .None
+            cell.favorIconImageView.hidden = !self.restaurantIsVisited[indexPath.row]
+        })
+        optionMenu.addAction(isVisitedAction)
+
+        // Display the menu
+        self.presentViewController(optionMenu, animated: true, completion: nil)
+        
+        // Deselect the cell
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+    }
+    
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            self.restaurantNames.removeAtIndex(indexPath.row)
+            self.restaurantLocations.removeAtIndex(indexPath.row)
+            self.restaurantTypes.removeAtIndex(indexPath.row)
+            self.restaurantIsVisited.removeAtIndex(indexPath.row)
+            self.restaurantImages.removeAtIndex(indexPath.row)
+
+            self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        }
+    }
+    
+    override func tableView(tableView: UITableView, editActionsForRowAtIndexPath indexPath: NSIndexPath) -> [AnyObject] {
+        
+        var shareAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Share", handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+
+            let shareMenu = UIAlertController(title: nil, message: "Share using", preferredStyle: .ActionSheet)
+            let twitterAction = UIAlertAction(title: "Twitter", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) in println("Foo")})
+            let facebookAction = UIAlertAction(title: "Facebook", style: UIAlertActionStyle.Default, handler: nil)
+            let emailAction = UIAlertAction(title: "Email", style: UIAlertActionStyle.Default, handler: nil)
+            let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+            
+            //shareMenu.addAction(twitterAction)
+            shareMenu.addAction(twitterAction)
+            shareMenu.addAction(facebookAction)
+            shareMenu.addAction(emailAction)
+            shareMenu.addAction(cancelAction)
+            
+            self.presentViewController(shareMenu, animated: true, completion: nil)
+            }
+        )
+        
+        var deleteAction = UITableViewRowAction(style: UITableViewRowActionStyle.Default, title: "Delete",handler: { (action:UITableViewRowAction!, indexPath:NSIndexPath!) -> Void in
+
+            // Delete the row from the data source
+            self.restaurantNames.removeAtIndex(indexPath.row)
+            self.restaurantLocations.removeAtIndex(indexPath.row)
+            self.restaurantTypes.removeAtIndex(indexPath.row)
+            self.restaurantIsVisited.removeAtIndex(indexPath.row)
+            self.restaurantImages.removeAtIndex(indexPath.row)
+
+            self.tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+
+            }
+        )
+        
+        shareAction.backgroundColor = UIColor(red: 255.0/255.0, green: 166.0/255.0, blue: 51.0/255.0, alpha: 1.0)
+        deleteAction.backgroundColor = UIColor(red: 51.0/255.0, green: 51.0/255.0, blue: 51.0/255.0, alpha: 1.0)
+
+        return [deleteAction, shareAction]
+    }
+
+    
+
+}
