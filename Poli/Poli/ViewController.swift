959018e4ed856cf55e84b4dc6b9ad338aa5aa6aa
cagayakemiracl <hirosnonno1182@gmail.com>
2015-02-24 23:30:25 +0900

    Poliプロジェクトの作成

---
+++ Poli/Poli/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Poli
+//
+//  Created by 中原 啓雅 on 2015/02/24.
+//  Copyright (c) 2015年 中原啓雅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
