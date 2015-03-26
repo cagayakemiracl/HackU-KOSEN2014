435fb8f59355dc356444768caaf6a958a3b5045a
cagayakemiracl <hirosnonno1182@gmail.com>
2015-03-26 18:35:23 +0900

    メイクの一つをとりあえず出来るようにした

--- FaceTracker/FaceTracker/ViewController.swift
+++ FaceTracker/FaceTracker/ViewController.swift
@@ -23,15 +23,104 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     // 出力先
     var myOutput : AVCaptureVideoDataOutput!
     var recognition = Recognition()
-    var imcImageController: ImageController!
+    var imcImageController = ImageController()
+
+    var processString = [String]()
+    var pointArray = [NSArray]()
+    var processIndex = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        imcImageController = ImageController()
         // Do any additional setup after loading the view, typically from a nib.
+        initProcessString()
+        initPointArray()
+        self.processLabel.text = self.processString[0]
         if initCamera() {
             mySession.startRunning()
         }
+    }
+
+    func initProcessString() {
+        // やはりWEBアプリにしてデータベースに保存したい
+        self.processString.append("化粧水などで肌を整える")
+        self.processString.append("BBクリームを5カ所に分けてまんべんなく塗る")
+        self.processString.append("ファンデーションでテカリを抑える")
+        self.processString.append("眉毛はナチュラルに仕上げるために髪より少し明るいカラーを選ぶ")
+        self.processString.append("アイラインのポイントは目頭の切開ラインと目尻のラインを長めにまたはねさせる")
+        self.processString.append("アイシャドウは濃いブラウンでラインをぼかす")
+        self.processString.append("明るいブラウンまたはカラー")
+        self.processString.append("ホワイト")
+        self.processString.append("3分の1程ブラウンのラインを引く")
+        self.processString.append("ホワイトを乗せて涙袋を強調する")
+        self.processString.append("まつ毛は全体を上げた後に目頭から目の中心のまつ毛だけを3段階に分け上げる")
+        self.processString.append("マスカラは目頭から目の中心のまつ毛を上に目尻側のまつ毛は流すように意識して付ける")
+        self.processString.append("下まつげにも程よくマスカラを乗せて")
+    }
+
+    func initPointArray() {
+        // やはりWEBアプリにしてデータベースに保存したい
+        var array:NSArray!
+        var p1: DrawPointInFace!
+        var p2: DrawPointInFace!
+        var p3: DrawPointInFace!
+        var p4: DrawPointInFace!
+        var p5: DrawPointInFace!
+
+        array = NSArray(array: [])
+        self.pointArray.append(array)
+
+        p1 = DrawPointInFace(21, x: 10, y: -20)
+        p2 = DrawPointInFace(2, x: 20, y: 0)
+        p3 = DrawPointInFace(29, x: 0, y: 0)
+        p4 = DrawPointInFace(14, x: -20, y: 0)
+        p5 = DrawPointInFace(8, x: 0, y: -10)
+        array = NSArray(array: [p1, p2, p3, p4, p5])
+        self.pointArray.append(array)
+
+        array = NSArray(array: [])
+        self.pointArray.append(array)
+
+        p1 = DrawPointInFace(19, x: 0, y: 0)
+        p2 = DrawPointInFace(24, x: 0, y: 0)
+        array = NSArray(array: [p1, p2])
+        self.pointArray.append(array)
+
+        p1 = DrawPointInFace(37, x: 0, y: 0)
+        p2 = DrawPointInFace(43, x: 0, y: 0)
+        array = NSArray(array: [p1, p2])
+        self.pointArray.append(array)
+
+        p1 = DrawPointInFace(36, x: 0, y: 0)
+        p2 = DrawPointInFace(45, x: 0, y: 0)
+        array = NSArray(array: [p1, p2])
+        self.pointArray.append(array)
+
+        p1 = DrawPointInFace(36, x: -10, y: -10)
+        array = NSArray(array: [p1])
+        self.pointArray.append(array)
+
+        p1 = DrawPointInFace(17, x: 0, y: 10)
+        array = NSArray(array: [p1])
+        self.pointArray.append(array)
+
+        p1 = DrawPointInFace(41, x: 10, y: 5)
+        array = NSArray(array: [p1])
+        self.pointArray.append(array)
+
+        p1 = DrawPointInFace(47, x: 5, y: 5)
+        array = NSArray(array: [p1])
+        self.pointArray.append(array)
+
+        array = NSArray(array: [])
+        self.pointArray.append(array)
+
+        p1 = DrawPointInFace(37, x: 0, y: -5)
+        array = NSArray(array: [])
+        self.pointArray.append(array)
+
+        array = NSArray(array: [])
+        self.pointArray.append(array)
+
     }
 
     // カメラの準備処理
@@ -119,14 +208,16 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     {
         // UIImageへ変換して表示させる
         var image = imcImageController.createImageFromBuffer(sampleBuffer)
-        var point1 = DrawPointInFace(0, x: 30.0, y: 0.0)
-        var point2 = DrawPointInFace(1, x: 30.0, y: 0.0)
-        var array = NSArray(array: [point1, point2])
-        image = recognition.Apply(image, drawPoints: array)
+        image = recognition.Apply(image, drawPoints: self.pointArray[self.processIndex])
 
         dispatch_async(dispatch_get_main_queue(), {
             self.view.layer.contents = image.CGImage
         })
+    }
+
+    func changeProcessLabel() {
+        self.processLabel.text = self.processString[self.processIndex]
+        self.processLabel.sizeToFit()
     }
 
     @IBAction func onClick(sender: UIButton) {
@@ -134,10 +225,12 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
 
     @IBAction func onClickNextBtn(sender: UIButton) {
-        //self.recognition.PrintPoints()
-        var points = recognition.GetPoints() as [AnyObject] as [NSNumber]
-        for point in points {
-            NSLog("%@", point)
-        }
+        self.processIndex += 1
+        changeProcessLabel()
+    }
+
+    @IBAction func onClickBackBtn(sender: UIButton) {
+        self.processIndex -= 1
+        changeProcessLabel()
     }
 }
